0f971552bd9b2c1b56787016e63e1c3678503317
JoelMXLinux <joel-oswald@outlook.com>
2019-10-29 20:22:52 +0100

    Workarounf for Prediction
    Hard Coded Endpoint and Keys, but at least it is working now....
    Weird though, that config is parsed correctly, but not working in file

--- examples/tutorials/01_hello_world.py
+++ examples/tutorials/01_hello_world.py
@@ -28,7 +28,7 @@ def main():
         print("Say 'Hello Seve Eleven'...")
         robot.behavior.say_text("Hello Seven Eleven")
         #robot.anim.play_animation("Seven Eleven")
-        robot.audio.stream_wav_file('./vector_shutdown.wav', volume=100)
+        robot.audio.stream_wav_file('vector_shutdown.wav', volume=100)
        
 
 if __name__ == "__main__":
--- find_object.py
+++ find_object.py
@@ -9,9 +9,15 @@ class img_prediction(object):
         config_parser = configparser.RawConfigParser()
         config_parser.read(config_file_path)
 
-        self.ENDPOINT = config_parser.get('CV', 'prediction_endpoint')
-        print(self.ENDPOINT)
+        self.ENDPOINT = config_parser.get('CustomVision', 'endpoint')
 
+
+
+#        self.training_key = config_parser.get('CustomVision', 'training_key')
+#        self.prediction_key = config_parser.get('CustomVision', 'prediction_key')
+#        self.prediction_resource_id = config_parser.get('CustomVision', 'ressource_id')
+#        self.publish_iteration_name = config_parser.get('CustomVision', 'publish_iteration_name')
+#        self.project_id = config_parser.get('CustomVision', 'project_id')
 
         self.training_key = config_parser.get('CustomVision', 'training_key')
         self.prediction_key = config_parser.get('CustomVision', 'prediction_key')
@@ -19,10 +25,13 @@ class img_prediction(object):
         self.publish_iteration_name = config_parser.get('CustomVision', 'publish_iteration_name')
         self.project_id = config_parser.get('CustomVision', 'project_id')
 
+        print('Endpoint: ', self.ENDPOINT)
+
 
         
         
-        self.predictor = CustomVisionPredictionClient(self.prediction_key, endpoint=self.ENDPOINT)
+        self.predictor = CustomVisionPredictionClient('627746c9a7084f74a448c0040ad15a32', 'https://joelcustomvision.cognitiveservices.azure.com/')
+    
         
         
 
@@ -31,18 +40,21 @@ class img_prediction(object):
     def predict(self, img_path):
         # Open the image and get back the prediction results as a dict with tuple (left, top, width, height)
         with open(img_path, mode="rb") as image_to_predict:
-            results = self.predictor.detect_image(self.project_id, self.publish_iteration_name, image_to_predict)
+            results = self.predictor.detect_image('002e7a08-8696-4ca8-8769-fe0cbc2bd9b0', "Iteration0.1", image_to_predict)
 
-        # Display the results. 
+        # Display the results, and return them as a dict (Tuple of four for ecery Tag) 
         tag_dict = dict()   
         for prediction in results.predictions:
             print("\t" + prediction.tag_name + 
             ": {0:.2f}% bbox.left = {1:.2f}, bbox.top = {2:.2f}, bbox.width = {3:.2f}, bbox.height = {4:.2f}".format(prediction.probability * 100, 
             prediction.bounding_box.left, prediction.bounding_box.top, prediction.bounding_box.width, prediction.bounding_box.height))
-            tag_dict[prediction.tagname] = (prediction.bounding_box.left, prediction.bounding_box.top, prediction.bounding_box.width, prediction.bounding_box.height)
+            if prediction.probability > 0.5:
+                tag_dict[prediction.tag_name] = (prediction.bounding_box.left, prediction.bounding_box.top, prediction.bounding_box.width, prediction.bounding_box.height)
         
         return tag_dict
 
 
 if __name__ == '__main__':
     prediction = img_prediction()
+    results = prediction.predict('./balloon_pic.jpg')
+    print(results)
